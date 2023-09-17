3fda3d4b42a76afc0a3f079ae4c87e604dfda3de
Samantha <ada_hack479@hackademy.mx>
2023-09-17 00:24:07 -0600

    JS: 03

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ console.log(`01. El área de un triángulo de base ${base} y altura ${altura} es
 let gradosCelsius = 20;
 let gradosFahrenheit = gradosCelsius * 9 / 5 + 32;
 console.log("02. " + gradosCelsius + "°C son " + gradosFahrenheit + "°F");
+
+//03. Encontrar el máximo de dos números.
+let num1 = 50;
+let num2 = 100;
+let numeroMaximo = Math.max(num1, num2);
+console.log("03. El máximo entre " + num1 +" y " + num2 + " es: " + numeroMaximo);
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@
 
 01. - [x] Calcular el área de un triángulo
 02. - [x] Convertir grados Celsius a Fahrenheit.
-03. - [ ] Encontrar el máximo de dos números.
+03. - [x] Encontrar el máximo de dos números.
 04. - [ ] Calcular el factorial de un número.
 05. - [ ] Verificar si un número es primo.
 06. - [ ] Revertir una cadena de texto.
